84f2bd5581809180cdc93f58091f5cbce2c55da9
bNolions <bnolions@h-tech.co>
2021-12-28 12:22:34 +0800

    [revsie] 修正 testing

--- template/RealTimeStrategyGame_test.go
+++ template/RealTimeStrategyGame_test.go
@@ -5,17 +5,9 @@ import "testing"
 func TestAgeOfEmpires(t *testing.T) {
 	a := &AgeOfEmpires{}
 	play(a)
-
-	t.Log("initialize")
-	t.Log("start")
-	t.Log("end")
 }
 
 func TestStarcraft(t *testing.T) {
 	a := &AgeOfEmpires{}
 	play(a)
-
-	t.Log("initialize")
-	t.Log("start")
-	t.Log("end")
 }
